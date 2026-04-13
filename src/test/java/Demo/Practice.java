39814d84e3b845b0dcaa9e5e2fcb340b906e0a3a
User <User@DESKTOP-EGFPNU6>
2026-04-13 09:35:36 +0530

    this is my third commit

--- src/test/java/Demo/Practice.java
+++ src/test/java/Demo/Practice.java
@@ -5,7 +5,8 @@ public static void main(String []args)
 {
 	System.out.println("Git Practice");
 
-	System.out.println("Nikhil");}
+	System.out.println("Nikhil");
 
-
+    System.out.println("karthi in the park");
+}
 }
